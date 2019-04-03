10e5d8958fe795d1917aad16abd85b6c8aa38aa8
Nevael <amaury.forest@etu.univ-lyon1.fr>
2019-04-03 14:38:13 +0200

    Modif arbo du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { QuestHeaderComponent } from './quest-header/quest-header.component';
-import { QuestListComponent } from './quest-list/quest-list.component';
-import { QuestionListComponent } from './question-list/question-list.component';
-import { QuestionComponent } from './question/question.component';
+import { QuestHeaderComponent } from './main-page/quest-header/quest-header.component';
+import { QuestListComponent } from './main-page/quest-list/quest-list.component';
+import { QuestionListComponent } from './main-page/question-list/question-list.component';
+import { QuestionComponent } from './main-page/question/question.component';
 
 @NgModule({
   declarations: [
